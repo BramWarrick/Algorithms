27e10c75142c2044142c1d48a3ff09494cd07594
BramWarrick <bram.warrick@gmail.com>
2016-12-28 09:35:46 -0800

    fix: changed variable names for clarity
    Changed variable names; added  and  for minor optimization, readability, and helped PEP line length.

--- Longest Palindromic Subsequence.py
+++ Longest Palindromic Subsequence.py
@@ -1,31 +1,33 @@
-def LPSubsequence(a):
+def LPSubsequence(str):
     """
     Returns the longest palindromic subsequence within the input string
 
-    This can be the result of dropping characters to achieve the longest palindrome
-    (e.g. `abdgda` will yield a result of `adgda` because dropping the `b` allows 
-    for the longest palindrome.)
+    This can be the result of dropping characters to achieve the
+    longest palindrome (e.g. `abdgda` will yield a result of `adgda`
+    because dropping the `b` allows for the longest palindrome.)
     """
-    if a is None or len(a) == 0:
+    if str is None or len(str) == 0:
         return ""
 
-    sl = len(a)     # sl is string length
+    sl = len(str)     # sl is string length
 
     # Create a table to store results of subproblems
     L = [[0 for x in range(sl)] for x in range(sl)]
 
     # Create palindrome of 1 for each character in input string (a)
     for i in range(sl):
-        L[i][i] = a[i]
+        L[i][i] = str[i]
 
     # cl is check string length
     for cl in range(2, sl + 1):
         for start in range(sl - cl + 1):
             stop = start + cl - 1
-            if a[start] == a[stop] and cl == 2:
-                L[start][stop] = a[start] * 2
-            elif a[start] == a[stop]:
-                L[start][stop] = a[start] + L[start + 1][stop - 1] + a[stop]
+            first = str[start]
+            last = str[stop]
+            if first == last and cl == 2:
+                L[start][stop] = first * 2
+            elif first == last:
+                L[start][stop] = first + L[start + 1][stop - 1] + last
             else:
                 L[start][stop] = LPSubsequenceLongest(
                     L[start][stop - 1], L[start + 1][stop])
@@ -42,12 +44,12 @@ def LPSubsequenceLongest(string1, string2):
 
 def LPSubsequenceLength(str):
     """
-    Returns the length of the longest palindromic subsequence within 
+    Returns the length of the longest palindromic subsequence within
     the input string.
 
-    This can be the result of dropping characters to achieve the longest 
-    palindrome (e.g. `abdgda` will yield a palindrome of `adgda` because 
-    dropping the `b` allows for the longest palindrome, resulting in a 
+    This can be the result of dropping characters to achieve the longest
+    palindrome (e.g. `abdgda` will yield a palindrome of `adgda` because
+    dropping the `b` allows for the longest palindrome, resulting in a
     returned value of 5.)
     """
     return len(LPSubsequence(str))
@@ -61,3 +63,6 @@ print LPSubsequence('ajsrdvv')
 print LPSubsequence('ajsrdvp')
 
 print LPSubsequenceLength('level')
+print LPSubsequenceLength('levl')
+print LPSubsequenceLength(None)
+print LPSubsequenceLength('')
