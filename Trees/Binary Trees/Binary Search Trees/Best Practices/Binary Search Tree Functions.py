779f15dfad48604485af0277a1733eda96bf281c
BramWarrick <bram.warrick@gmail.com>
2017-01-09 13:55:48 -0800

    feat: Binary Search Tree - Added  method
    Added  method to the Tree Node class, with fix to ensure data is written even if a key of that value already exists.

--- Trees/Binary Trees/Binary Search Trees/Best Practices/Binary Search Tree Functions.py
+++ Trees/Binary Trees/Binary Search Trees/Best Practices/Binary Search Tree Functions.py
@@ -61,3 +61,27 @@ class TreeNode:
         if self.hasRightChild():
             self.rightChild.parent = self
 
+    def put(self, key, val):
+        if self.root:
+            self._put(key, val, self.root)
+        else:
+            self.root = TreeNode(key, val)
+        self.size = self.size + 1
+
+    def _put(self, key, val, currentNode):
+        if key < currentNode.key:
+            if currentNode.hasLeftChild():
+                self._put(key, val, currentNode.leftChild)
+            else:
+                currentNode.leftChild = TreeNode(
+                    key, val, parent=currentNode)
+        elif key > currentNode.key:
+            if currentNode.hasRightChild():
+                self._put(key, val, currentNode.rightChild)
+            else:
+                currentNode.rightChild = TreeNode(
+                    key, val, parent=currentNode)
+        else:
+            self.replaceNodeData(key, val, currentNode.leftChild,
+                                 currentNode.rightChild)
+
