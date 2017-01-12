87eeaa8ca131e3d08c3d253da4254defdab45ab2
BramWarrick <bram.warrick@gmail.com>
2017-01-12 15:21:07 -0800

    feat: Added minimum Binary Search Tree
    Current: Node, Tree and insert. Deletion is being mapped and has a comment/scaffolding.

--- Trees/Binary Trees/Binary Search Trees/Best Practices/Spartan Node, Binary Search Tree.py
+++ Trees/Binary Trees/Binary Search Trees/Best Practices/Spartan Node, Binary Search Tree.py
@@ -10,9 +10,41 @@ class BinarySearchTree:
     def __len__(self):
         return self.size
 
+    def insert(self, val):
+        if self.root is None:
+            self.root = Node(val)
+            self.size += 1
+        else:
+            current = self.root
+            while True:
+                if val == current.data:
+                    return
+                elif val < current.data:
+                    if current.lefchild is None:
+                        current.leftChild = Node(val)
+                        self.size += 1
+                        return
+                    else:
+                        current = current.leftChild
+                else:
+                    if current.rightChild is None:
+                        current.rightChild = Node(val)
+                        self.size += 1
+                        return
+                    else:
+                        current = current.rightChild
 
-class TreeNode:
+class Node:
     def __init__(self, val, left=None, right=None):
-        self.val = val
+        self.data = val
         self.leftChild = left
         self.rightChild = right
+
+
+t = BinarySearchTree()
+t.insert(4)
+t.insert(5)
+print t.root.data #this works
+print t.size
+print t.root.rightChild.data
+# print t.root.rchild.data #this works too
